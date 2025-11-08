14f4c728945cf997e2e813d6b8aeae50c1ebb158
Lavanya-SureshBabu <lavipooja75@gmail.com>
2025-11-08 22:51:25 +0530

    Add files via upload
    Week1/day1/Chrome

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float versionoftype = 91.0f;
+		
+		String developeroftype = "Google";
+		
+		boolean isbetaoftype = false;
+		
+		int releaseYearoftype = 2008;
+		
+		char shortcutKeyoftype = 'C';
+		
+		
+	System.out.println(versionoftype);
+	
+	System.out.println(developeroftype);
+	
+	System.out.println(isbetaoftype);
+	
+	System.out.println(releaseYearoftype);
+	
+	System.out.println(shortcutKeyoftype);
+
+	}
+
+}
---
+++ week1/day1/HelloTestLeaf.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	
+	public static void main (String[]args) {
+		
+		//byte age = 26;
+		
+		//short ageOfAnElephant = 128;
+		
+		//int valueOf100KgPotato = 40000;
+		
+		//long mobileNumber = 989499805409040940l;
+		
+		//float CGPA = 9.84034803940f;
+		
+		//double rateOfA105kgAatta = 1.598786;
+		
+		//char nameOfOrg = 'S';
+		
+		//String name = "Lavanya";
+		
+		boolean didyouundestandjava = True;
+		
+		System.out.println(didyouundestandjava);
+	}
+
+}
